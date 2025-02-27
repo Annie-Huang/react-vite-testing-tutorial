33c9e35b08415a28178a911db4f5d11b041af433
Annie Huang <annie.hn@gmail.com>
2025-02-27 22:23:07 +1100

    Add setupFiles in vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -4,5 +4,7 @@ export default defineConfig({
   test: {
     // https://vitest.dev/config/#environment
     environment: 'jsdom',
+    // https://stackoverflow.com/questions/77611978/invalid-chai-property-in-vitest
+    setupFiles: ['./setupTests.js'],
   },
 });
