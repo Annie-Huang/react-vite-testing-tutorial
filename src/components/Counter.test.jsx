378353744db0bcb618daaea8ba04c1a3403126ba
Annie Huang <annie.hn@gmail.com>
2025-02-28 21:58:54 +1100

    Add use click button test

--- src/components/Counter.test.jsx
+++ src/components/Counter.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Counter from './Counter.jsx';
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 
 describe('Counter', () => {
   it('increments counter on button click', async () => {
@@ -11,5 +12,13 @@ describe('Counter', () => {
     const counterValue = screen.getByTestId('counter-value');
 
     expect(counterValue.textContent).toEqual('0');
+
+    await userEvent.click(button);
+
+    expect(counterValue.textContent).toEqual('1');
+
+    await userEvent.click(button);
+
+    expect(counterValue.textContent).toEqual('2');
   });
 });
