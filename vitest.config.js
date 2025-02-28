331d7e0f92694ee747254ef22ab10390ceeaa014
Annie Huang <annie.hn@gmail.com>
2025-02-28 21:28:36 +1100

    Fixed 'ReferenceError: expect is not defined' error

--- vitest.config.js
+++ vitest.config.js
@@ -6,5 +6,9 @@ export default defineConfig({
     environment: 'jsdom',
     // https://stackoverflow.com/questions/77611978/invalid-chai-property-in-vitest
     setupFiles: ['./setupTests.js'],
+    // https://stackoverflow.com/questions/75971024/describe-is-not-defined-in-vitest
+    // https://vitest.dev/config/#globals
+    // Otherwise, you will keep getting 'ReferenceError: expect is not defined' error
+    globals: true,
   },
 });
